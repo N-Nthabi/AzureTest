c8b201b259aea64ee1e924f87437ee94d5d90c44
Nthabi <nthabisen@gmail.com>
2021-04-03 13:48:18 +0200

    lab 2

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const mainRouter = require('./mainRoutes')
+
+app.use(mainRouter)
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('Express server running on port', port)
---
+++ mainRoutes.js
@@ -0,0 +1,11 @@
+'use strict'
+const path = require('path')
+const express = require('express')
+const mainRouter = express.Router()
+mainRouter.get('/', function (req, res) {
+  res.send('Hello World Im node.js')
+})
+mainRouter.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+module.exports = mainRouter
